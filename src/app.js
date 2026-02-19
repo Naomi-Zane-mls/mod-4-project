c3c5c39550cedafd5aa38df86c8f38ccc58c1952
[NaomiS-ara] <[Naomispencer101@gmail.com]>
2026-02-19 11:34:14 -0500

    second-push-basic-form-event-handling

--- src/app.js
+++ src/app.js
@@ -1,36 +1,22 @@
 
-import { fetchCollection } from './api.js'; // pulls in the fetch function i wrote in api.js
 
-async function init() { // async means the function can use await inside it. It calls fetchCollection() and waits for it to finish before moving on, that's what await does.
-  const items = await fetchCollection();
-  if (items) render(items);
+
+export async function init(facts) { // async means the function can use await inside it. It calls fetchCollection() and waits for it to finish before moving on, that's what await does.
+  if (facts.data) render(facts);
 }
 
-function render(items) { // grabs the ul element that already exists in the html. where the list items will be injected
-  const ul = document.querySelector('ul');
+export function render(facts) { // grabs the ul element that already exists in the html. where the list items will be injected
+  const factList = document.querySelector('#cat-fact-list');
 
-  items.forEach(item => { // loops over every item in the array one at a time
+  facts.data.forEach(fact => { // loops over every item in the array one at a time
     const li = document.createElement('li'); // creates a new li element in memory, its not on the page (yet)
-    li.dataset.id = item.id; // sets the data-id attribute on the li. dataset.id is the JS way of writing data-id in html. li data-id="123" this lets me identift which item was clicked
-    li.textContent = item.name; //put fields for the API returns
-    ul.appendChild(li); // adds the li to the ul on the page. everything before this was building it in memory, this is the line that makes it appear
+    li.dataset.id = fact.id; // sets the data-id attribute on the li. dataset.id is the JS way of writing data-id in html. li data-id="123" this lets me identift which item was clicked
+    li.textContent = fact; //put fields for the API returns
+    factList.appendChild(li); // adds the li to the ul on the page. everything before this was building it in memory, this is the line that makes it appear
   });
 }
 
-init(); // calls init when the file runs
 
 
 // form event handling
-const form = document.querySelector('#generate-form');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault(); // stops the browsers default form bahavior which would refresh the page
-
-  const factCount = document.querySelector('#fact-count').value;
-
-  console.log({ factCount });
-
-  form.reset();
-});
-
-init();
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { renderSingleFact } from "./dom-helpers";
 import { getSingleFact } from "./single-fact-fetch";
-
+import { fetchCollection } from "./fetch-collection";
+import { init } from "./app";
+import { render } from "./app";
 const errorMessage = document.querySelector("#error-message")
 const successMessage = document.querySelector('#success-message')
 const singleFactButton = document.querySelector('#single-fact-button')
-
+const form = document.querySelector('#generate-form');
 
 const getAndRenderSingleFact = async () => {
     const factObj = await getSingleFact()
@@ -21,3 +23,20 @@ singleFactButton.addEventListener('click', () => {
     getAndRenderSingleFact();
 })
 
+async function getAndRenderCollection(num) {
+    const facts = await fetchCollection(num)
+    if (facts.error) {
+        errorMessage.textContent = `Error: ${factObj}`
+        return;
+    }
+    console.log(facts)
+    render(facts)
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); // stops the browsers default form behavior which would refresh the page
+
+  const factCount = document.querySelector('#fact-count').value;
+    getAndRenderCollection(factCount)
+  form.reset();
+});
