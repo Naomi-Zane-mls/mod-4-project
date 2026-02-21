9899c72a599c4175bd2e282d38f7f0f99caefc68
ZaneC04 <zenithzsc@gmail.com>
2026-02-21 12:23:39 -0500

    feature/polish-and-organization

--- src/dom-helpers.js
+++ src/dom-helpers.js
@@ -1,4 +1,19 @@
 export const renderSingleFact = (fact) => {
     const factH2 = document.querySelector("#single-fact")
-    factH2.textContent = `Did you know?: ${fact}`
+    factH2.textContent = `${fact}`
+}
+
+export async function init(facts) { 
+  if (facts.data) render(facts);
+}
+
+export function render(facts) { 
+  const factList = document.querySelector('#fact-list');
+
+  facts.data.forEach(fact => { 
+    const li = document.createElement('li'); 
+    li.dataset.id = fact.id; 
+    li.textContent = fact; 
+    factList.appendChild(li);
+  });
 }
