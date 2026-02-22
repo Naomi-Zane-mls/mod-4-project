26d462cc74758c174d58716d219a892fd24ccb40
ZaneC04 <zenithzsc@gmail.com>
2026-02-21 21:31:41 -0500

    feature/sfx

--- src/main.js
+++ src/main.js
@@ -2,18 +2,32 @@ import { renderSingleFact } from "./dom-helpers";
 import { getSingleFact } from "./single-fact-fetch";
 import { fetchCollection } from "./fact-collection-fetch";
 import { render, renderFavorites } from "./dom-helpers"; 
-import { addFavorite } from "./favorites";               
+import { addFavorite } from "./favorites";                
+import { clearStaleFavorites } from "./favorites";
+
+
 
 const errorMessage = document.querySelector("#error-message");
 const singleFactButton = document.querySelector('#single-fact-button');
 const singleFactEl = document.querySelector('#single-fact');
 const form = document.querySelector('#generate-form');
+const singleMeow = new Audio('sfx/singlemeow.mp3')
+const multipleMeow = new Audio('sfx/multiplemeow.mp3')
+const favoriteMeow = new Audio('sfx/favoritemeow.mp3')
+const muteButton = document.querySelector('#mute-button')
+let isMuted = false;
 
+
+muteButton.addEventListener('click', () => {
+   isMuted = !isMuted
+})
 
 singleFactEl.addEventListener("click", () => {
   const text = singleFactEl.textContent.trim();
   if (!text) return;
-
+    if (isMuted === false) {
+        favoriteMeow.play()
+    }
   addFavorite(text);
   renderFavorites();
   singleFactEl.style.color = "var(--calico_orange)";
@@ -25,7 +39,6 @@ const getAndRenderSingleFact = async () => {
         errorMessage.textContent = `Error: ${factObj}`
         return;
     }
-    console.log(factObj)
     renderSingleFact(factObj.data)
     singleFactEl.style.color = ""
 }
@@ -35,6 +48,9 @@ getAndRenderSingleFact()
 singleFactButton.addEventListener('click', () => {
     document.querySelector('#fact-list').style.display = 'none'
     singleFactEl.style.display = 'grid'
+   if (isMuted === false) {
+    singleMeow.play()
+   }
     getAndRenderSingleFact();
 })
 
@@ -52,11 +68,15 @@ form.addEventListener('submit', (e) => {
   const factCount = document.querySelector('#fact-count').value;
   singleFactEl.style.display = 'none'
   document.querySelector('#fact-list').style.display = 'grid'
-    getAndRenderCollection(factCount)
+  if (isMuted === false) {
+    multipleMeow.play() 
+   }
+  getAndRenderCollection(factCount)
   form.reset();
 });
 
-renderFavorites();
 clearStaleFavorites();
+renderFavorites();
+
 
 
